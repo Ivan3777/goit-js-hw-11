8652cb390ad255c1c13da9f0073b826900e6643f
Ivan Vodopianov <vis3010@gmail.com>
2022-09-23 16:28:46 +0300

    add input & searchQuery.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Notiflix from "notiflix";
+import simpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import './sass/index.css';
+import NewApiImageService from "./searchQuery";
+
